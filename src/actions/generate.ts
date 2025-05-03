585d0f58f966f45572a40748e7a6dd8e15b376af
Ayoub El Haddadi <ayoub1998elh@gmail.com>
2025-05-03 20:43:02 +0100

    Added .env file generation command

--- src/actions/generate.ts
+++ src/actions/generate.ts
@@ -1,5 +1,6 @@
 import { IOption } from "../Interface/Option";
 import { exec as execCB } from "child_process";
+import { appendFileSync, existsSync, readFileSync, writeFileSync } from "fs";
 import { promisify } from "util";
 
 export const generateCommand = (opts: Array<IOption>) => {
@@ -7,6 +8,13 @@ export const generateCommand = (opts: Array<IOption>) => {
     const keysOpts = opts.find((o) => o.name === "keys" || o.name == "k");
     if (keysOpts) {
         generateKeys();
+        return;
+    }
+
+    //generate .env file
+    const envOpts = opts.find((o) => o.name === "env" || o.name === "e");
+    if (envOpts) {
+        generateEnv();
         return;
     }
 };
@@ -18,4 +26,22 @@ const generateKeys = async () => {
     console.log("Creating Public Key...");
     await exec(`openssl rsa -pubout -in private.key -out public.pem`);
     console.log("Done.");
+};
+
+const generateEnv = () => {
+    let content = readFileSync(`${__dirname}/../templates/dotenv.txt`, "utf8");
+    writeFileSync(`./.env`, content);
+    writeFileSync(`./.env.example`, content);
+
+    // check if a .gitignore exists
+    if (existsSync("./.gitignore")) {
+        // if exists check if contains .env
+        const gitigonre = readFileSync(".gitignore", "utf8");
+        // if not append .env
+        if (!/(^|\s)\.env(?=\s|$)/.test(gitigonre)) {
+            appendFileSync(".gitignore", "\n.env");
+            console.log(".env has been appended to .gitignore file");
+        }
+    }
+    console.log("Environment files has been created");
 };
